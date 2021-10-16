68226a1f6fbeac54f74b41a239c9a72dfe880fb4
Ryan <vandelin586@gmail.com>
2021-10-16 18:48:39 -0400

    trying pandas

--- dataanalysis/getdata.py
+++ dataanalysis/getdata.py
@@ -1,1 +1,6 @@
-import pandas
+import pandas as pd
+pd.set_option('display.max_rows', 1000)
+#with pd.option_context('display.max_rows', 100, 'display.max_columns', None):
+df = pd.read_csv(
+    'fivelist.txt',header=None)
+print(df)
