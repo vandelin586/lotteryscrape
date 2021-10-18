e34c1f15b827fb0649c9e449267fc8a871f73cd6
Ryan <vandelin586@gmail.com>
2021-10-18 09:06:38 -0400

    set pd options to not truncate

--- dataanalysis/getdata.py
+++ dataanalysis/getdata.py
@@ -1,5 +1,10 @@
+from sys import displayhook
 import pandas as pd
-pd.set_option('display.max_rows', 1000)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', -1)
+#pd.set_option('display.max_rows', 1000)
 #with pd.option_context('display.max_rows', 100, 'display.max_columns', None):
 df = pd.read_csv(
     'fivelist.txt',header=None)
