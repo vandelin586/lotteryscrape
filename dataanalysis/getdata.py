299fc314454030d69e3a93e0e813e473fe9c38ad
Ryan <vandelin586@gmail.com>
2021-10-18 09:14:31 -0400

    clean up

--- dataanalysis/getdata.py
+++ dataanalysis/getdata.py
@@ -4,8 +4,6 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', -1)
-#pd.set_option('display.max_rows', 1000)
-#with pd.option_context('display.max_rows', 100, 'display.max_columns', None):
 df = pd.read_csv(
     'fivelist.txt',header=None)
 print(df)
