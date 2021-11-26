a8825288ad44a9c470e2970471ef81d20c59f47f
Ryan <vandelin586@gmail.com>
2021-11-25 21:25:40 -0500

    updated

--- dataanalysis/getdata.py
+++ dataanalysis/getdata.py
@@ -1,9 +1,8 @@
 from sys import displayhook
 import pandas as pd
 pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', -1)
-df = pd.read_csv(
-    'fivelist.txt',header=None)
+#pd.set_option('display.max_columns', None)
+#pd.set_option('display.width', None)
+#pd.set_option('display.max_colwidth', -1)
+df = pd.read_csv('fivelist.txt',header=None)
 print(df)
