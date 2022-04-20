9292e11155f9622cc95c41bf4d193a603501024f
Ryan M Vandelinder <vandelinderrpm@yahoo.com>
2022-04-19 20:44:23 -0400

    0419update

--- webscraper.py
+++ webscraper.py
@@ -1,33 +1,35 @@
-#ouput_lst = This is a list of the element(Table body) found on the website that contains the line with lottery numbers listed on it.
-#allnum = All Mega Millions lottery number from start of year
-#powerfile = The Powerball number since beginning of year
-#Element2 = the search for the lottery numbers posted on Website
-#test
+# ouput_lst = This is a list of the element(Table body) found on the website that contains the line with lottery numbers listed on it.
+# allnum = All Mega Millions lottery number from start of year
+# powerfile = The Powerball number since beginning of year
+# Element2 = the search for the lottery numbers posted on Website
+# test
 
-import re 
+import re
 from typing import Type
 from selenium import webdriver
 
 output_lst = []
 fivefile = open("fivelist.txt", "a")
 powerfile = open("powerlist.txt", "a")
-url = 'https://lottery.com/previous-results/mi/megamillions/2021/','https://lottery.com/previous-results/mi/megamillions/2020/','https://lottery.com/previous-results/mi/megamillions/2019/','https://lottery.com/previous-results/mi/megamillions/2018/','https://lottery.com/previous-results/mi/megamillions/2017/'
-allnum = open("allnums.txt", "a")   
+url = 'https://lottery.com/previous-results/mi/megamillions/2021/', 'https://lottery.com/previous-results/mi/megamillions/2020/', 'https://lottery.com/previous-results/mi/megamillions/2019/', 'https://lottery.com/previous-results/mi/megamillions/2018/', 'https://lottery.com/previous-results/mi/megamillions/2017/'
+allnum = open("allnums.txt", "a")
 
-def urlfunc (echurl):     
+
+def urlfunc(echurl):
     for x in echurl:
-        print(x)      
+        print(x)
         browser = webdriver.Chrome()
         browser.get(x)
-        for tr in browser.find_elements_by_xpath('/html/body/main/section/div/div/table/tbody') :
+        for tr in browser.find_elements_by_xpath('/html/body/main/section/div/div/table/tbody'):
             tds = tr.find_elements_by_tag_name('tr')
             output_lst = [tr.text for tr in tds]
             browser.quit
 
             for element in output_lst:
-                element2 = re.search("((\d*)-(\d*)-(\d*)-(\d*)-(\d*)-(\d*))",element) 
+                element2 = re.search("((\d*)-(\d*)-(\d*)-(\d*)-(\d*)-(\d*))", element)
                 allnum.write(element2.group() + "\n")
-                powerfile.write(element2.group (7) + "\n")
+                powerfile.write(element2.group(7) + "\n")
                 fivefile.write(element2.group(2) + "\n" + element2.group(3) + "\n" + element2.group(4) + "\n" + element2.group(5) + "\n" + element2.group(6) + "\n")
-    
-urlfunc (url)
+
+
+urlfunc(url)
