45c904193b99da9d4ece0e57f6d9bc430c4deaf0
ryan v <vandelinderrpm@yahoo.com>
2022-08-03 20:59:30 -0400

    recreate tables,rows

--- webscraper.py
+++ webscraper.py
@@ -1,20 +1,20 @@
+
 # Python program to demonstrate
 # selenium
- 
-# import webdriver
+# # import webdriver
 from selenium import webdriver
  
 # create webdriver object
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
  
 # enter keyword to search
-keyword = "geeksforgeeks"
+keyword = "lottery"
  
 # get geeksforgeeks.org
-driver.get("https://www.geeksforgeeks.org/")
+driver.get("https://www.lotteryusa.com/michigan/mega-millions/year")
  
 # get element
-element = driver.find_element_by_xpath("//form[input/@name ='search']")
+element = driver.find_elements("by.xpath" '/html/body/main/div[3]/form/div[2]/div/div/div/table/tbody/tr[2]/th/time')
  
 # print complete element
 print(element)
