74c25fd667263c6409b6af0b926d13ca0f46d09e
ryan v <vandelinderrpm@yahoo.com>
2022-08-05 17:55:58 -0400

    update

--- webscraper.py
+++ webscraper.py
@@ -17,12 +17,13 @@ driver.get("https://www.lotteryusa.com/michigan/mega-millions/year")
  
 # get element
 #element = driver.find_element(By.XPATH, '//*[@id="main"]/div[3]/form/div[2]/div/div/div/table/tbody/tr[2]/th/time').text
-element = driver.find_element(By.XPATH, '//*[@id="main"]/div[3]/form/div[2]/div/div/div/table/tbody/tr[]/th/time')
+element = driver.find_element(By.XPATH, '/html/body/main/div[3]/form/div[2]/div/div/div/table/tbody/tr[5]/th').accessible_name
+#element = driver.find_element(By.XPATH, '//*[@id="main"]/div[3]/form/div[2]/div/div/div/table/tbody/tr[]/th/time')
 #driver.find_element(By.XPATH, '//button[text()="Some text"]')
 # print complete element
 print(element)
 
-
+#get this text //*[@id="main"]/div[3]/form/div[2]/div/div/div/table/tbody/tr[4]...
 #.../(By.xpath(".//*.getText();
 
 #driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[2]/article/div/table/tbody/tr["+sRow+"]/td["+sCol+"]")).getText();
